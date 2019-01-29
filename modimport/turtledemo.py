c5e277b9579f2c08aca22db48dce0c7a715e8e9d
anandkumarjhaic <37099338+anandkumarjhaic@users.noreply.github.com>
2019-01-29 18:27:44 +0530

    Time basics in Python

---
+++ modimport/turtledemo.py
@@ -0,0 +1,13 @@
+
+import turtle
+done = "well done, You have finished your drawing"
+
+
+
+turtle.forward(150)
+turtle.right(250)
+turtle.forward(150)
+turtle.circle(80)
+turtle.done()
+print(done)
+
